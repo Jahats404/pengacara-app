80c712f1146ccc90046be964b777799b06f471ca
Jahats404 <fajrikmal141@gmail.com>
2024-12-01 23:13:10 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,7 @@ Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('authenticate');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/action-register', [AuthController::class, 'action_register'])->name('action-register');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
     
@@ -33,12 +35,12 @@ Route::middleware(['auth'])->group(function () {
     // Route prefix untuk Produsen
 
     Route::prefix('admin')->name('admin.')->middleware('CekUserLogin:1')->group(function () {
-
+        Route::get('/dashboard', [DashboardController::class, 'admin'])->name('dashboard');
     });
     
 
-    Route::prefix('pengawas')->name('pengawas.')->middleware('CekUserLogin:2')->group(function () {
-
+    Route::prefix('client')->name('client.')->middleware('CekUserLogin:2')->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'client'])->name('dashboard');
     });
 
 });
